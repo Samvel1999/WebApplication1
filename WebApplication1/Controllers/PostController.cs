74ce8affdca76b9f7019d68b424dafcaaecd1d99
samvelkhachatryan1999 <samvelkhachatryan1999@gmail.com>
2025-03-22 20:21:52 +0400

    Add project files.

---
+++ WebApplication1/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+
+namespace WebApplication1.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class PostController
+    {
+        private readonly PostService _postService;
+
+        public PostController(PostService postService)
+        {
+            _postService = postService;
+        }
+
+        // GET: api/posts?userId=1&title=qui%20est%20esse
+        [HttpGet("posts")]
+        public async Task<ActionResult<List<Post>>> GetPosts([FromQuery] int? userId, [FromQuery] string title)
+        {
+            var posts = await _postService.GetPostsAsync(userId, title);
+
+            // Return the posts
+            return posts;
+        }
+    }
+}
---
+++ WebApplication1/Services/PostService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class PostService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PostService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Post>> GetPostsAsync(int? userId, string title)
+        {
+            // Build the query string based on the parameters
+            var query = "?";
+
+            if (userId.HasValue)
+            {
+                query += $"userId={userId.Value}&";
+            }
+
+            if (!string.IsNullOrEmpty(title))
+            {
+                query += $"title={Uri.EscapeDataString(title)}&";
+            }
+
+            // Remove the trailing '&' or '?' if no valid parameters
+            if (query.EndsWith("&"))
+            {
+                query = query.Remove(query.Length - 1);
+            }
+
+            var url = "https://jsonplaceholder.typicode.com/posts" + query;
+
+            // Call the external API
+            var response = await _httpClient.GetStringAsync(url);
+
+            // Deserialize the JSON response into a list of Post objects
+            var posts = JsonConvert.DeserializeObject<List<Post>>(response);
+
+            return posts;
+        }
+    }
+}
